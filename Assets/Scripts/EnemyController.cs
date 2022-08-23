5cfe5809152e2b34f15aa5bf527e8c181b16ed65
Gezuriya <pelmen7892@gmail.com>
2022-08-23 22:24:14 +0300

    Obstacles script and location minor updates

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    void Update()
+    Vector3 StartPos;
+    bool Check;
+    public float speed;
+    private void Start()
     {
-        
+        StartPos = transform.position;
+        if (StartPos.x < 0)
+            Check = true;
+        else if (StartPos.x > 0)
+            Check = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (Check)
+        {
+            transform.Translate(speed * new Vector3(1,0,0) * Time.deltaTime, Space.World);
+            transform.rotation = Quaternion.Euler(0, 90, 0);
+        }
+        else
+        {
+            transform.Translate(speed * new Vector3(-1, 0, 0) * Time.deltaTime, Space.World);
+            transform.rotation = Quaternion.Euler(0, -90, 0);
+        }
     }
 }
